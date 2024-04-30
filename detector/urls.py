570e60fe39b43e76eb97801ce4c420e319508c73
Aarti-04 <aarti.sharma2727981@gmail.com>
2024-04-30 18:17:07 +0530

    adding compose mail feature

--- detector/urls.py
+++ detector/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Predict,GoogleAuthVerify,MailOperation,RegisterAuthVerify
+from .views import Predict,GoogleAuthVerify,RegisterAuthVerify,MailRead,ComposeMail
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path("predict/",Predict.as_view(),name="predict"),
     path("register/",RegisterAuthVerify.as_view(),name="login"),
     path("google-auth-verify/",GoogleAuthVerify.as_view(),name="googleAuth"),
-    path("mailoperation",MailOperation.as_view(),name="MailOperation")
+    # path("mailoperation",MailOperation.as_view(),name="MailOperation"),
+    path("mailread/",MailRead.as_view(),name="mailread"),
+    path("composemail/",ComposeMail.as_view(),name="ComposeMail")
+
 
 ]
--- detector/views.py
+++ detector/views.py
@@ -39,11 +39,53 @@ import random
 import string
 from django.contrib.auth.hashers import make_password
 from rest_framework.exceptions import ValidationError
+from django.contrib.auth import authenticate,login,logout
+import base64
+from email.message import EmailMessage
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.auth import default
+from google.auth.exceptions import DefaultCredentialsError
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import base64
+import json
+from google.oauth2.credentials import Credentials
+
+
+# from rest_framework.views import APIView, Response
+# from django.http import JsonResponse
+# import os
+# import json
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from googleapiclient.errors import HttpError
+# import googleapiclient.discovery
+# from google.oauth2 import service_account
+# import requests
+# from dotenv import load_dotenv
+# import nltk
+# import email
+# from nltk.corpus import stopwords
+# from nltk.stem import WordNetLemmatizer
+# import re
+# import jwt
+# import random
+# import string
+# from django.contrib.auth.hashers import make_password
+# from rest_framework.exceptions import ValidationError
+# from django.contrib.auth import authenticate, login, logout
+# from email.message import EmailMessage
+
 # nltk.download('stopwords')
 # Create your views here.
 load_dotenv()
-
-
 
 class RegisterAuthVerify(APIView):
     def post(self, request):
@@ -57,18 +99,18 @@ class RegisterAuthVerify(APIView):
         try:
             user_serializer.is_valid(raise_exception=True)
             user_serializer.save()
-            print("Registered sucessfully")
+            print("user_serializer",user_serializer)
+            # login(request,user_serializer)
             return Response("User Registered successfully ",status.HTTP_201_CREATED)
         except Exception as e:
-            print("email is not valid")
-            return Response("This email is already registered", status=status.HTTP_400_BAD_REQUEST)
-       
+            print(f"64 Error {str(e)} ")
+            return Response("Email already exist")
+            # raise ValidationError(str(e))
     def validate_token(self,id_token):
         r = customRequest.get(
             "https://www.googleapis.com/oauth2/v3/tokeninfo",
             params={"id_token": id_token}
         )
-        print("<>r<>",r)
         r.raise_for_status()
     def generate_random_password(self,email):
         random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
@@ -88,59 +130,6 @@ class RegisterAuthVerify(APIView):
                 "email": data["email"],
                 "name": data.get("name")
             }
-
-# views.py
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from .serializers import CustomeUserSerializer
-
-# class UserRegistrationView(APIView):
-#     def post(self, request):
-#         try:
-#             auth_credentials = request.data.get("creds")
-#             print("<>auth_credentials<>",auth_credentials)
-#             if auth_credentials:
-#                 id_token = auth_credentials.get("id_token")
-#                 if id_token:
-#                     user_data = self.get_decoded_data(id_token)
-#                     user_serializer = CustomeUserSerializer(data=user_data)
-#                     if user_serializer.is_valid():
-#                         user_serializer.save()
-#                         return Response("User Registered successfully", status=201)
-#                     else:
-#                         return Response(user_serializer.errors, status=400)
-#             return Response("Invalid request", status=400)
-#         except ValidationError as e:
-#             return Response("Error..........", status=400)
-
-#     def get_decoded_data(self, id_token):
-#         data = jwt.decode(id_token, options={"verify_signature": False})
-#         return {
-#             "password": self.generate_random_password(data["email"]),
-#             "email": data["email"],
-#             "name": data.get("name")
-#         }
-
-#     def generate_random_password(self, email):
-#         random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-#         password_base = email.split('@')[0] + random_string
-#         return password_base
-
-
-
-
-# class LoginAuthVerify(APIView):
-#     def post(self,request):
-#         data=request.body
-#         data_string = data.decode('utf-8')
-#         auth_credentials = json.loads(data_string)
-#         # print(auth_credentials)
-#         print(auth_credentials["creds"])
-#         print(auth_credentials["creds"]["access_token"])
-#         id_info = id_token.verify_oauth2_token(auth_credentials["creds"]["id_token"], requests.Request(), '"189496678458-fpihrhl6pae85mhtq0tsra89cpguccja.apps.googleusercontent.com"')
-        
-#         print(id_info)
-#         return Response("loginsuccess",status=status.HTTP_200_OK)
 
 class GoogleAuthVerify(APIView):
     
@@ -194,13 +183,7 @@ class GoogleAuthVerify(APIView):
         lemmatizer = WordNetLemmatizer()
         clean = [lemmatizer.lemmatize(word) for word in text]
         return clean
-
-    def get_body_content(self,parts):
-           for part in parts:
-            #    print("<>part<>",part)
-               if part['mimeType'] == 'text/plain':
-                    data = part['body']['data']
-                    # text=base64.urlsafe_b64decode(data).decode()
+      # text=base64.urlsafe_b64decode(data).decode()
                     # text = self.remove_html_tags(text)
                     # text = self.remove_punctuation(text)
                     # text = self.lowercase_text(text)
@@ -209,148 +192,19 @@ class GoogleAuthVerify(APIView):
                     # print("body.......")
                     # print(" ".join(text))
                     # return " ".join(text)
+
+    def get_body_content(self,parts):
+           for part in parts:
+            #    print("<>part<>",part)
+               if part['mimeType'] == 'text/plain':
+                    data = part['body']['data']
                     return base64.urlsafe_b64decode(data).decode()
                elif part['mimeType'] == 'multipart/mixed' or 'multipart/alternative':
                    return self.get_body_content(part['parts'])
-           return ''
-    def get_credentials(self,code):
-        # Implement user authorization flow or environment variable retrieval here
-        # Following lines are for illustrative purposes only (DO NOT USE DIRECTLY)
-        client_id = os.getenv("client_id")
-        client_secret = os.getenv("client_secret")
-        url = "https://oauth2.googleapis.com/token"
-        payload = {
-            "grant_type": "authorization_code",
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "redirect_uri":"http://127.0.0.1:8000/api/google-auth-verify",
-            "code": code
-        }
-        headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        response = customRequest.request("POST", url, headers=headers, data=payload)
-        print(response.text)
-        credentials=json.loads(response.text)
-        return Credentials(token=credentials["access_token"])
-    
-    def get_gmail_api_service(self):
-        credentials = self.get_credentials()
-        http = credentials.authorize(http())
-        return build('gmail', 'v1', http=http)
-
-    def read_subject_and_body(self,service, message_id):
-        try:
-            message = service.users().messages().get(userId='me', id=message_id).execute()
-            payload = message['payload']
-            headers = payload['headers']
-
-            # Extract subject
-            subject = None
-            for header in headers:
-                if header['name'] == 'Subject':
-                    subject = header['value']
-                    break
-
-            # Extract body
-            body = self.get_body_content(payload.get('parts', []))
-
-            return subject, body
-        except (KeyError, HttpError) as e:
-            print(f"Error retrieving message {message_id}: {e}")
-            return None, None
-
-    def get(self,request):
-        # code = request.GET.get('code1')
-        code =request.GET.get("code")
-        print(code)
-        # self.get_credentials(code)
-        url = "https://oauth2.googleapis.com/token"
-        payload = {
-            "grant_type": "authorization_code",
-            "client_id": "189496678458-lbsabcd97iss894bi6c5tjmnrv1e3vh8.apps.googleusercontent.com",
-            "client_secret": "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",
-            "redirect_uri":"http://127.0.0.1:8000/api/google-auth-verify",
-            "code": code
-        }
-        headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        response = customRequest.request("POST", url, headers=headers, data=payload)
-        print(response.text)
-
-        credentials=json.loads(response.text)
-        print("credential",credentials)
-        credentials = Credentials(token=credentials["access_token"])
-        print("credential......",credentials)
-        service = build('gmail', 'v1', credentials=credentials)
-        # print("<<<<<<service",service)
-        query = 'label:Inbox'  # You can set search criteria here (e.g., 'label:unread')
-
-        # Get a list of message IDs
-        response = service.users().messages().list(userId='me', q=query).execute()
-        messages = response.get('messages', [])
-
-        # Process a limited number of messages
-        max_results = 20  # Adjust as needed
-        results = []
-
-        for i, message in enumerate(messages[:max_results]):
-            msg_id = message['id']
-            # self.read_mail_with_content(service,msg_id)
-
-
-            # Get the full message details
-            full_message = service.users().messages().get(userId='me', id=msg_id).execute()
-            payload = full_message['payload']
-            headers = payload['headers']
-
-            # Extract subject
-            subject = None
-            for header in headers:
-                if header['name'] == 'Subject':
-                    subject = header['value'].rstrip()
-                    break           
-
-            # Extract the body parts
-            try:
-                parts = full_message['payload']['parts']
-                # print("<>parts<>",parts)
-                body = self.get_body_content(parts)
-                results.append({'id': msg_id, 'header':subject,'body': body})
-            except (KeyError, HttpError) as e:
-                print(f"Error retrieving message {msg_id}: {e}")
-        request.session["mail"]="aarti"
-        print(request.session["mail"])
-        return Response(results)
+           return '' 
     def post(self,request):
-        # code = request.GET.get('code1')
-        # print(request.body)
-        # code =request.GET.get("code")
-        # data=request.body
-        # data_string = data.decode('utf-8')
-        # auth_credentials = json.loads(data_string)
-        # print(auth_credentials["code"])
-        # code =auth_credentials["code"]
-        # # self.get_credentials(code)
-        # url = "https://oauth2.googleapis.com/token"
-        # payload = {
-        #     "grant_type": "authorization_code",
-        #     "client_id": "189496678458-lbsabcd97iss894bi6c5tjmnrv1e3vh8.apps.googleusercontent.com",
-        #     "client_secret": "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",
-        #     "redirect_uri":"http://127.0.0.1:8000/api/google-auth-verify",
-        #     "code": code
-        # }
-        # headers = {
-        # 'Content-Type': 'application/x-www-form-urlencoded'
-        # }
-        # response = customRequest.request("POST", url, headers=headers, data=payload)
-        # print(response.text)
-
-        # credentials=json.loads(response.text)
-        # print("credential",credentials)
-        # print(request.body)
-        code =request.GET.get("code")
+        lable_query =request.GET.get("querylable")
+        print(lable_query)
         data=request.body
         data_string = data.decode('utf-8')
         auth_credentials = json.loads(data_string)
@@ -359,14 +213,11 @@ class GoogleAuthVerify(APIView):
         credentials = Credentials(token=auth_credentials["access_token"])
         # print("credential......",credentials)
         service = build('gmail', 'v1', credentials=credentials)
-        print("<<<<<<service",service)
-        query = 'label:Inbox'  # You can set search criteria here (e.g., 'label:unread')
+        query = f'label:{lable_query}'  # You can set search criteria here (e.g., 'label:unread')
 
         # Get a list of message IDs
         response = service.users().messages().list(userId='me', q=query).execute()
         messages = response.get('messages', [])
-        # print("<><>",messages)
-        # Process a limited number of messages
         max_results = 10  # Adjust as needed
         results = []
 
@@ -396,12 +247,102 @@ class GoogleAuthVerify(APIView):
             except (KeyError, HttpError) as e:
                 print(f"Error retrieving message {msg_id}: {e}")
         return Response(results)
+class MailRead(APIView):
+    def get_body_content(self, parts):
+        if not parts:
+            return ''
+        for part in parts:
+            if part.get('mimeType') == 'text/plain':
+                data = part['body']['data']
+                return base64.urlsafe_b64decode(data).decode()
+            elif part.get('mimeType') == 'text/html':
+                data = part['body']['data']
+                return base64.urlsafe_b64decode(data).decode()
+            elif part.get('mimeType') in ('multipart/mixed', 'multipart/alternative'):
+                return self.get_body_content(part.get('parts', []))
+        return ''
+    def fetch_emails(self, service, query, max_results=10):
+        try:
+            response = service.users().messages().list(userId='me', q=query).execute()
+            messages = response.get('messages', [])
+            results = []
 
-class MailOperation(APIView):
-    def get(self,request):
-        inBoxMail=request.session.get("mail")
-        print(inBoxMail)
-        return Response(inBoxMail)
+            for message in messages[:max_results]:
+                msg_id = message['id']
+                full_message = service.users().messages().get(userId='me', id=msg_id).execute()
+                payload = full_message['payload']
+                headers = payload['headers']
+                subject = next((header['value'] for header in headers if header['name'] == 'Subject'), None)
+                print(subject)
+                parts = payload.get('parts', [])
+                body = self.get_body_content(parts)
+                # body = self.get_body_content(payload['parts'])
+                results.append({'id': msg_id, 'header': subject, 'body': body})
+                
+            return results
+        except HttpError as e:
+            print(f"Error fetching emails: {e}")
+            return []
+    def post(self, request):
+        lable_query = request.GET.get("querylable")
+        data = request.body
+        data_string = data.decode('utf-8')
+        auth_credentials = json.loads(data_string)
+        access_token = auth_credentials["access_token"]
+
+        credentials = Credentials(token=access_token)
+        service = build('gmail', 'v1', credentials=credentials)
+        query = f'label:{lable_query}'
+
+        results = self.fetch_emails(service, query)
+
+        return Response(results)
+class ComposeMail(APIView):
+    def post(self, request):
+        try:
+            data = request.body
+            data_string = data.decode('utf-8')
+            credentials = json.loads(data_string)
+            credentials["client_secret"]="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"
+            credentials["client_id"]="189496678458-fpihrhl6pae85mhtq0tsra89cpguccja.apps.googleusercontent.com"
+
+            # Initialize credentials using access token
+            creds = Credentials(token=credentials["access_token"])
+            scopes = ['https://www.googleapis.com/auth/gmail.compose']
+            creds1 = Credentials.from_authorized_user_info(credentials, scopes=scopes)
+            print(creds1)
+            # Build Gmail service
+            service = build("gmail", "v1", credentials=creds)
+
+            # Get user's email address
+            user_info = service.users().getProfile(userId='me').execute()
+            user_email = user_info['emailAddress']
+            # return Response(user_email)
+            # Construct the email message
+            message = MIMEMultipart()
+            message['to'] = "sharma.aart.dcs24@vnsgu.ac.in"
+            message['from'] = user_email
+            message['subject'] = "Automated draft"
+
+            # Add email body
+            body = "This is automated draft mail"
+            message.attach(MIMEText(body, 'plain'))
+
+            # Encoded message
+            raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+            # Create draft message
+            draft_message = {"message": {"raw": raw_message}}
+            draft = service.users().drafts().create(userId="me", body=draft_message).execute()
+
+            return Response({"message": "Mail draft created successfully"})
+        
+        except DefaultCredentialsError:
+            return Response({"error": "Authentication error. Ensure you have valid credentials."})
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return Response({"error": "Failed to create mail draft"})
 class Predict(APIView):
     def post(self,request):
         data = request.body
